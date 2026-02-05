c3d741d6fe8c192cf9e412fb2fcee770a69c184c
Darren Gideon <sutantodarren@gmail.com>
2026-02-05 21:33:12 +0700

    SEO needs

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -47,9 +47,9 @@ const social: Social = [
 
 const home: Home = {
   path: "/",
-  image: "/images/og/home.jpg",
+  image: "/images/og/home1.png",
   label: "Home",
-  title: `${person.name}'s Portfolio`,
+  title: `Cupcake-Legend's Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
   headline: <>Connecting ideas to code </>,
   featured: {
